3c6700c0a2c4cfd2e56859dd0e380f975d577390
Marc-Andre <marcadenis@msn.com>
2020-05-26 21:25:02 -0400

    fix after merge

--- test/test_solution_bas_niveau.c
+++ test/test_solution_bas_niveau.c
@@ -15,19 +15,20 @@ void strlen_should_return_length_of_string(){
     TEST_ASSERT(strlen("asdf") == 4);
 }
 
-void hexstr_should_return_unsigned_number_as_string(){
+void hexstr_should_return_unsigned_number_as_string() {
     char strTest[100] = {0};
-    hexstr(0x12345678,strTest,32);
-    TEST_ASSERT_EQUAL_STRING("12345678",strTest);
-    hexstr(0x12345678,strTest,16);
-    TEST_ASSERT_EQUAL_STRING("5678",strTest);
-    hexstr(0x12345678,strTest,8);
-    TEST_ASSERT_EQUAL_STRING("78",strTest);
+    hexstr(0x12345678, strTest, 32);
+    TEST_ASSERT_EQUAL_STRING("12345678", strTest);
+    hexstr(0x12345678, strTest, 16);
+    TEST_ASSERT_EQUAL_STRING("5678", strTest);
+    hexstr(0x12345678, strTest, 8);
+    TEST_ASSERT_EQUAL_STRING("78", strTest);
 
-    hexstr(0xAAFFEECC,strTest,32);
-    TEST_ASSERT_EQUAL_STRING("AAFFEECC",strTest);
-    hexstr(0x0,strTest,16);
-    TEST_ASSERT_EQUAL_STRING("0000",strTest);
+    hexstr(0xAAFFEECC, strTest, 32);
+    TEST_ASSERT_EQUAL_STRING("AAFFEECC", strTest);
+    hexstr(0x0, strTest, 16);
+    TEST_ASSERT_EQUAL_STRING("0000", strTest);
+}
 
 void test_strcpy_should_return_same_string(void) {
     char str[10]={};
