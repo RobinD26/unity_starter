a130dcbb9f9106127578cddd98b32e93b5813fc5
Marc-Andre <marcadenis@msn.com>
2020-05-26 21:02:53 -0400

    tests for strlen and hexstr

--- test/test_solution_bas_niveau.c
+++ test/test_solution_bas_niveau.c
@@ -9,18 +9,32 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_function_should_doBlahAndBlah(void) {
-    //test stuff
+void strlen_should_return_length_of_string(){
+    TEST_ASSERT(strlen("") == 0);
+    TEST_ASSERT(strlen("asdf") == 4);
 }
 
-void test_function_should_doAlsoDoBlah(void) {
-    //more test stuff
+void hexstr_should_return_unsigned_number_as_string(){
+    char strTest[100] = {0};
+    hexstr(0x12345678,strTest,32);
+    TEST_ASSERT_EQUAL_STRING("12345678",strTest);
+    hexstr(0x12345678,strTest,16);
+    TEST_ASSERT_EQUAL_STRING("5678",strTest);
+    hexstr(0x12345678,strTest,8);
+    TEST_ASSERT_EQUAL_STRING("78",strTest);
+
+    hexstr(0xAAFFEECC,strTest,32);
+    TEST_ASSERT_EQUAL_STRING("AAFFEECC",strTest);
+    hexstr(0x0,strTest,16);
+    TEST_ASSERT_EQUAL_STRING("0000",strTest);
 }
 
 // not needed when using generate_test_runner.rb
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_function_should_doBlahAndBlah);
-    RUN_TEST(test_function_should_doAlsoDoBlah);
+
+    RUN_TEST(strlen_should_return_length_of_string);
+    RUN_TEST(hexstr_should_return_unsigned_number_as_string);
+
     return UNITY_END();
 }
