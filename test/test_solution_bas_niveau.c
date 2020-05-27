8704bebc2ac366903e8535e2e2ffae4bfc73d355
RobinD26 <robin.dupuis.1@ens.etsmtl.ca>
2020-05-26 21:18:13 -0400

    test longtodec

--- test/test_solution_bas_niveau.c
+++ test/test_solution_bas_niveau.c
@@ -1,26 +1,29 @@
 #include "unity/unity.h"
 #include "../src/solution_c_bas_niveau.h"
 
+
 void setUp(void) {
+
     // set stuff up here
+
 }
 
 void tearDown(void) {
+
     // clean stuff up here
+
 }
 
-void test_function_should_doBlahAndBlah(void) {
-    //test stuff
-}
-
-void test_function_should_doAlsoDoBlah(void) {
-    //more test stuff
+void test_longtodec(void) {
+    char str2[20] = {0};
+    TEST_ASSERT_EQUAL_STRING ("12345", longtodec(12345,str2));
+    TEST_ASSERT_EQUAL_STRING ("0", longtodec(0,str2));
 }
 
 // not needed when using generate_test_runner.rb
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_function_should_doBlahAndBlah);
-    RUN_TEST(test_function_should_doAlsoDoBlah);
+    RUN_TEST(test_longtodec);
+    //RUN_TEST(test_function_should_doAlsoDoBlah);
     return UNITY_END();
 }
